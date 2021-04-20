a3849a39aa1d1618c8702fd94198802997cca0a6
Nahid-Eraz <nahidulislameraz@gmail.com>
2021-04-20 13:10:41 +0600

    book page done

--- src/components/DashBoard/BookRent/BookRent.js
+++ src/components/DashBoard/BookRent/BookRent.js
@@ -5,7 +5,34 @@ const BookRent = (props) => {
     const { name, charge, imageURL } = props.bookRent;
 
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+    
+    const onSubmit = data => {
+        const bookRentData = {
+            key: Math.floor(Math.random() * 100000000000000000000).toString(),
+            name: data.name,
+            email: data.email,
+            phone: data.phone,
+            address: data.address,
+            category: name,
+            charge: charge,
+            date: new Date().toDateString()
+        };
+        console.log(bookRentData)
+
+        const url = `http://localhost:5005/dashboard/bookings`;
+        fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify(bookRentData)
+        })
+        .then(res => res.json())
+        .then(success =>{
+            if(success){
+                alert('Rent booking successfully')
+                
+            }
+        })
+    };
 
     return (
         <div className="row d-flex align-items-center">
@@ -23,10 +50,18 @@ const BookRent = (props) => {
                     <input {...register("address")} placeholder="Address" required />
                     <br />
                     <br />
+                    <form action="/action_page.php">
+                        <input Name type="radio" name="payment" id="1" value="Master Card"/>
+                        <label for="card"> Master Card</label>
+                        <input className="ms-4" type="radio" name="payment" id="2" value="bKash"/>
+                        <label for="mBank"> bKash</label>
+                    </form>
+                    <br />
+                    <br />
                     <input className="btn btn-main" type="submit" />
                 </form>
             </div>
-            <div className="col-md-6 img-fluid">
+            <div className="col-md-3 img-fluid">
                 <img src={imageURL} alt="" />
                 <h4 className="text-warning">Category: {name}</h4>
                 <h4>Require <span className="text-warning">${charge}</span> per hour</h4>
